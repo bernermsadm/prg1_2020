198b494f3ebba9fdfec6ae6da2b88c26e744b60f
bernermsadm <mberner@mail.ch>
2020-11-14 09:19:48 +0100

    Das ist ein Test, ob ich weiss, ob ich richtig liege

---
+++ src/ch/teko/prg/tag01/work/Test.java
@@ -0,0 +1,23 @@
+package ch.teko.prg.tag01.work;
+
+import javax.print.DocFlavor;
+
+public class Test {
+
+
+public static void hello(String name, String secondname, String drittername) {
+    System.out.printf("Hello, " +name+ " and " + secondname+ "\n und zu gut der letzt \n trommelwirbel........." + drittername);
+}
+
+    public static void main(String[] args)    {
+
+    hello("Matthias", "Peter","Manfred");
+
+
+
+
+
+    }
+
+
+}
